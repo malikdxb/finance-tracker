747edf7b0763432e3d7babcfacf425a76b670487
malikdxb <107611944+malikdxb@users.noreply.github.com>
2022-08-11 11:36:35 +0400

    create and display stock object in browser

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,11 @@
-//= require jquery
+//= require jquery3
+//= require popper
+//= require bootstrap
 //=require jquery_ujs
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import jquery from 'jquery'
+window.jQuery = jquery
+window.$ = jquery
 import "bootstrap"
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,6 @@ class Stock < ApplicationRecord
     def self.new_lookup(ticker_symbol)
         client = IEX::Api::Client.new(publishable_token: 'Tpk_857a36b7f8354558b344b53ba5b911bb',
                                                 endpoint: 'https://sandbox.iexapis.com/v1')
-        client.price(ticker_symbol)
+        new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol) )
         end
 end
